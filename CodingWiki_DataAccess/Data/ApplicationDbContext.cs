66f9c9c31a4e462d1fac728a3803b4ff431eed87
VijayHYadav <vizzyadav1997@gmail.com>
2023-04-29 16:04:30 +0530

    9. Create ApplicationDbContext and Book Model

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
+
+/*
+ * 9. Create ApplicationDbContext and Book Model
+ * 
+ * This DB context is responsible to provide all the logic that is needed for entity framework core.
+ * Like if we have to access a database, we want to retrieve, update, delete something or we have to create tables.
+ * 
+ * DB set: DB set will basically be responsible or rather be the classes of the tables that we want in our application.
+ * whatever name we give to our DB set will be the name of the table that gets added in database.
+ * 
+ * 
+ */
