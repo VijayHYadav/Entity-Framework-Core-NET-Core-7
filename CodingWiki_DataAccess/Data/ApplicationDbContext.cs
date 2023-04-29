4d50cd0285444fcdae4e2cf60c353564ab5e8621
VijayHYadav <vizzyadav1997@gmail.com>
2023-04-29 19:33:37 +0530

    14. Migration Snapshot

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -32,6 +32,22 @@ namespace CodingWiki_DataAccess.Data
  * 
  * right now we have created the table for our book model in our database, and we applied the migration
  * 
+ * 14. Migration Snapshot
+ * 
+ * EFMigrationsHistory: you can see we have the migration name that was applied.
+ * Right here we have the migration name that was applied as well as the Dotnet core version.
+ * Main thing to notice here is the migration ID. This is how entity framework code will track which migrations 
+ * have been applied on the database. That way, if we add a new migration, it will not apply the migration that
+ * has already been applied on the database and it will automatically apply only the migrations that have not 
+ * been pushed to the database.
+ *  
+ * Here it keeps a snapshot of what has been created in database. That way when we add a migration, it knows 
+ * that hey, in database these are the columns and if a new 
+ * column is added in, let's say the book model, it will know that the column does not exist in database.Based
+ * on the snapshot. And then it will add that in the new migration. That is how EF core will track all the
+ * changes that are there between the current code and the database.
+ * When we practice this, we add more migration, we update more database, everything will make much sensce
+
  */
 
 /**
