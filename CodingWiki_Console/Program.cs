3141fab638913bd744e5c4030b6e9f6adb950c2f
VijayHYadav <vizzyadav1997@gmail.com>
2023-05-01 13:52:26 +0530

    53. Database Helper Methods

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodingWiki_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+using(ApplicationDbContext context = new())
+{
+    context.Database.EnsureCreated();
+
+    if(context.Database.GetPendingMigrations().Count() > 0)
+    {
+        context.Database.Migrate();
+    }
+}
+
+
+/**
+ * 53. Database Helper Methods
+ * 
+ * EnsureCreated: This will do is if your database is not created, when the execution comes over here, it will
+ * first find the connection string and it will try to determine whether the database exists or not.
+ * The ensure created here will create the database if that does not exist.Our database already exist, so nothing will 
+ * happen even if this is executed.
+ *
+ * GetPendingMigrations(): This will do is it will get all the migrations that have not yet been applied to the database.
+ * And if the migrations are not applied, we can use the method context.Database.Migrate(); to apply all the pending
+ * migrations to database. This way when we run the application, if any migrations are pending, it will automatically be
+ * applied to the database.
+ * 
+ */
