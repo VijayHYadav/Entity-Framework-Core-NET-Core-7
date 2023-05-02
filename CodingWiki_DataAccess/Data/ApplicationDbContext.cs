27b49a483735ee3a6a673cf464b4d81a04a84aea
VijayHYadav <vizzyadav1997@gmail.com>
2023-05-02 07:09:34 +0530

    71. Retrieve using EF Core & 72. Create and Update Get Action Method

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace CodingWiki_DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
-        public DbSet<Category> Genres { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<BookAuthorMap> BookAuthorMaps { get; set; }
---
+++ CodingWiki_Web/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new();
+            if (id == null || id == 0)
+            {
+                // create
+                return View(obj);
+            }
+            // edit
+            obj = _db.Categories.First(u=>u.CategoryId== id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+    }
+}
